ae4cca6141c374746fed0990643396b4ee23ebad
Kevin Zúñiga <kevin.zun@gmail.com>
2018-05-03 11:39:25 -0500

    Add some environment variables to use later

--- publish.sh
+++ publish.sh
@@ -9,10 +9,13 @@ fi
 echo "Removing existing files"
 rm -rf public/*
 
+# Export some useful variables
+export LAST_COMMIT=$(git log --format="%h" -n 1)
+export LAST_UPDATED=$(TZ=UTC git log -local --date=format-local:"%Y-%m-%d %H:%M" --format="%ad" -n 1)
+export REPO_URL="https://$(git config --get remote.origin.url | sed "s/\(^git@\|\.git$\)//g" | sed "s/:/\//")"
+
 echo "Generating site"
 hugo
 
-commit=$(git log --format="%h" -n 1)
-
 echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages ($commit)"
+cd public && git add --all && git commit -m "Publishing to gh-pages ($LAST_COMMIT)"
